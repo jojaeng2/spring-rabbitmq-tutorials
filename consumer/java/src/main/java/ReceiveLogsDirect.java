88c9898c6ef324247a4d201725a1527f5e9613f5
jojaeng2 <ds4ouj@naver.com>
2022-08-08 17:03:13 +0900

    (2022-08-08) RabbitMQ Routing tutorials

---
+++ consumer/java/src/main/java/ReceiveLogsDirect.java
@@ -0,0 +1,39 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ReceiveLogsDirect {
+
+    private static final String EXCHANGE_NAME = "direct_logs";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+        String queueName = channel.queueDeclare().getQueue();
+        ArrayList<String> arr = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+        String sev = in.nextLine();
+        arr.add(sev);
+        arr.add("info");
+
+        for (String severity : arr) {
+            channel.queueBind(queueName, EXCHANGE_NAME, severity);
+        }
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" +
+                    delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+    }
+}
--- producer/java/src/main/java/EmitLog.java
+++ producer/java/src/main/java/EmitLog.java
@@ -8,19 +8,20 @@ public class EmitLog {
 
     private static final String EXCHANGE_NAME = "logs";
 
-    public static void main(String[] argv) throws Exception {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
-            channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
-            Scanner in = new Scanner(System.in);
-            String message = in.nextLine();
-            if(message.length() < 1) {
-                message = "info: Hello World!";
-            }
-            channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
-            System.out.println(" [x] Sent '" + message + "'");
-        }
-    }
+//    public static void main(String[] argv) throws Exception {
+//        ConnectionFactory factory = new ConnectionFactory();
+//        factory.setHost("localhost");
+//        try (Connection connection = factory.newConnection();
+//             Channel channel = connection.createChannel()) {
+//            channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+//            Scanner in = new Scanner(System.in);
+//            String message = in.nextLine();
+//            if(message.length() < 1) {
+//                message = "info: Hello World!";
+//            }
+//            String severity = in.nextLine();
+//            channel.basicPublish(EXCHANGE_NAME, severity, null, message.getBytes("UTF-8"));
+//            System.out.println(" [x] Sent '" + message + "'");
+//        }
+//    }
 }
